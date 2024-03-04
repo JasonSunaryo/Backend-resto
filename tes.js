31284c026fb21a502eebdd83e7f263e54ab59e64
JasonSunaryo <jasonsunaryo01@gmail.com>
2024-03-04 17:03:05 +0700

    menambah menu

--- tes.js
+++ tes.js
@@ -1,15 +1,37 @@
 $(document).ready(function () {
-    $('.caption').click(function () {
-      $('.kotakmetode').show();
-    });
-  
-    $('.kotakmetode').click(function (event) {
-      if (event.target !== this) return; // Jangan menutup popup jika kontennya yang diklik
-      $('.kotakmetode').hide();
-    });
-  
-    $('.popupCloseButton').click(function () {
-      $('.kotakmetode').hide();
-    });
+  $('.caption1').click(function () {
+    $('.kotakmetode').show();
   });
-  
+
+  $('.kotakmetode').click(function (event) {
+    if (event.target !== this) return; // Jangan menutup popup jika kontennya yang diklik
+    $('.kotakmetode').hide();
+  });
+
+  $('.popupCloseButton').click(function () {
+    $('.kotakmetode').hide();
+  });
+
+  $('.caption2').click(function () {
+    $('.kotakmetode2').show();
+  });
+  $('.kotakmetode2').click(function (event) {
+    if (event.target !== this) return; // Jangan menutup popup jika kontennya yang diklik
+    $('.kotakmetode2').hide();
+  });
+  $('.popupCloseButton').click(function () {
+    $('.kotakmetode2').hide();
+  });
+
+  $('.caption3').click(function () {
+    $('.kotakmetode3').show();
+  });
+  $('.kotakmetode3').click(function (event) {
+    if (event.target !== this) return; // Jangan menutup popup jika kontennya yang diklik
+    $('.kotakmetode3').hide();
+  });
+  $('.popupCloseButton').click(function () {
+    $('.kotakmetode3').hide();
+  });
+
+});
